41cddac5f4911f3bc268c411e8bd5c30aac8d6d1
francislo1221 <francislo1221@gmail.com>
2018-12-22 11:22:20 -0800

    render with random row from database when page refreshed

--- server/index.js
+++ server/index.js
@@ -1,14 +1,48 @@
 const express = require('express');
 let app = express();
+const path = require('path');
 const bodyParser = require('body-parser');
+const db = require('../db/index.js');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "properties"
+})
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(path.join(__dirname + '/../client/dist')));
 
 let port = 4000;
+// app.get('./:listingID', function(req, res) {
+//   res.send(200).sendFile(path.join(__dirname, '../client/dist.index.html'))
+// })
 
-app.get('./:listingID', function(req, res) {
-  res.send(200).sendFile(path.join(__dirname, '../client/dist.index.html'))
+// app.get('/listings', function(req, res) {
+//   db.getRandomListing(function(err, results) {
+//     if (err) {
+//       console.log(err)
+//       return;
+//     } else {
+//       res.send(results);
+//     }
+//   });
+// });
+app.get('/listings', (req, res) => {
+  var num = Math.floor(Math.random() * 30);
+  console.log(num)
+  var queryParams = [num];
+
+  con.query('select * from listings where id = ?', queryParams, function(err, results) {
+    if (err) {
+      console.log(err)
+      return;
+    } else {
+      console.log(JSON.stringify(results))
+      res.send(JSON.stringify(results));
+    }
+  })
 })
 
 app.listen(port, function() {
