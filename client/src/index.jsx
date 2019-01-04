0b0265884bed7fd3d6a7d04714a02d6b0c3d7c46
francislo1221 <francislo1221@gmail.com>
2019-01-03 16:51:04 -0800

    edit listingId

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,6 @@ import App from './components/App.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var listingId = window.location.pathname.slice(-1);
+var listingId = window.location.pathname.slice(1);
 console.log(listingId)
 ReactDOM.render(<App id = {listingId}/>, document.getElementById('header'));
